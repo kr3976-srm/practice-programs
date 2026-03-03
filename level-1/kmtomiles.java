e4d9399a8a11830c984e289a84add4ee4ed8076e
Krishanth R J <krishanth.raya@gmail.com>
2026-03-03 10:10:37 +0530

    Problem 3 : Program to convert distance kilometer to miles

---
+++ level-1/kmtomiles.java
@@ -0,0 +1,8 @@
+public class kmtomiles {
+    public static void main(String[] args) {
+        // placeholder for km to miles conversion
+        double kilometer = 10.8;
+        double miles = kilometer * 1.6;
+        System.out.println("The distance " + kilometer + " km in miles is : "  + miles);
+    }
+}

43f8c757979d0fbc1dc4ba93c39110a3a7886330
Krishanth R J <krishanth.raya@gmail.com>
2026-03-03 10:10:40 +0530

    Problem 2 : Finding average percent mark in PCM

---
+++ level-1/average.java
@@ -0,0 +1,7 @@
+public class average {
+    public static void main(String[] args) {
+        int Maths = 94, Physics = 95, Chemistry = 96;
+        double average = (Maths + Physics + Chemistry) / 3.0;
+        System.out.println("Average of Maths, Physics, and Chemistry is: " + average);
+    }
+}
